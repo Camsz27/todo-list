2b119a4e0c74fdf4ca6beb8c4cad556de5139f55
Camsz27 <camiloagr2707@gmail.com>
2021-07-17 19:20:25 -0500

    Added file with images and made header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
+import "./style.css";
+
 console.log("working");
+const container = document.querySelector("#content");
+const title = document.createElement("h1");
+title.textContent = "TodoList";
+//container.append(title);
