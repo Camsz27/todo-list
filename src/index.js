ed96df606113bcaddd7e28dea9ddbcf00b55e136
Camsz27 <camiloagr2707@gmail.com>
2021-08-05 13:46:30 -0500

    Added the icons to the dist folder

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import "./style.css";
-import taskFactory from "./task";
-import project from "./project";
-import display from "./display";
-import displayProject from "./sideBar";
+import './style.css';
+import addIcon from './images/add.svg';
+import deleteIcon from './images/delete.svg';
+import editIcon from './images/edit.svg';
+import taskFactory from './task';
+import project from './project';
+import display from './display';
+import displayProject from './sideBar';
 
-const task1 = taskFactory("Clean house", "Aug 30");
-const task2 = taskFactory("Do Homework", "Aug 08");
-const task3 = taskFactory("Take dog for a walk", "Aug 13");
-const task4 = taskFactory("Buy groceries", "Aug 17");
+const task1 = taskFactory('Clean house', 'Aug 30');
+const task2 = taskFactory('Do Homework', 'Aug 08');
+const task3 = taskFactory('Take dog for a walk', 'Aug 13');
+const task4 = taskFactory('Buy groceries', 'Aug 17');
 
-const project1 = project.projectFactory("Project 1");
+const project1 = project.projectFactory('Project 1');
 project1.addTask(task1);
 project1.addTask(task2);
-const project2 = project.projectFactory("Project 2");
+const project2 = project.projectFactory('Project 2');
 project2.addTask(task3);
 project2.addTask(task4);
 
@@ -21,8 +24,8 @@ displayProject(project2);
 start();
 
 function start() {
-  const projects = document.querySelector("ul.projectList");
+  const projects = document.querySelector('ul.projectList');
   const start = projects.children[0];
-  start.classList.add("active");
+  start.classList.add('active');
   display.changeProject(project1);
 }
